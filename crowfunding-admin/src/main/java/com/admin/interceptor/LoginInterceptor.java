4c95ce23e6a595deb8f0e3ce57fe36ed6a0a27cb
kongnang <brucejiu123@gmail.com>
2022-02-23 22:38:10 +0800

    删除自定义登录拦截器

--- crowfunding-admin/src/main/java/com/admin/interceptor/LoginInterceptor.java
+++ crowfunding-admin/src/main/java/com/admin/interceptor/LoginInterceptor.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
  * @author qiu
  * @create 2021-12-29 19:36
  */
+@Deprecated
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
