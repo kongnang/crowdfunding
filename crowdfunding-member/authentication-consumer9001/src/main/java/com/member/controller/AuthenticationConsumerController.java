c6c4bb9161f9f082a74eef151ae19ccc51d0acef
kongnang <brucejiu123@gmail.com>
2022-03-08 22:35:03 +0800

    增加首页、注册页、登录页的跳转映射

--- crowdfunding-member/authentication-consumer9001/src/main/java/com/member/controller/AuthenticationConsumerController.java
+++ crowdfunding-member/authentication-consumer9001/src/main/java/com/member/controller/AuthenticationConsumerController.java
@@ -18,8 +18,30 @@ public class AuthenticationConsumerController {
     @Autowired
     private MemberFeignService memberFeignService;
 
+    /**
+     * 登录页面
+     * @return
+     */
+    @RequestMapping("/login")
+    public String loginPage(){
+        return "member-login";
+    }
+
+    /**
+     * 注册页面
+     * @return
+     */
+    @RequestMapping("/register")
+    public String registerPage(){
+        return "member-register";
+    }
+
+    /**
+     * 首页
+     * @return
+     */
     @RequestMapping("/")
-    public String showIndexPage(){
+    public String indexPage(){
         return "index";
     }
 
